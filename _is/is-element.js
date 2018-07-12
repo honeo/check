32bacfcdab9185daf8f9eea4a6a542c85d558c58
honeo <hone.mk2+github@gmail.com>
2018-07-13 03:03:35 +0900

    fix: isElement sono2

--- _is/is-element.js
+++ _is/is-element.js
@@ -4,7 +4,7 @@
 const isObject = require('./is-object.js');
 
 function isElement(arg){
-	return isObject(arg) && arg instanceof Element;
+	return arg instanceof Element;
 }
 
 module.exports = isElement;
